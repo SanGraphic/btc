df1d96eda1bc065568e6c5207ab1cfd5496abb80
san <44402346+SanGraphic@users.noreply.github.com>
2025-03-15 11:26:13 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,22 @@
-const CACHE_NAME = 'btc-cache-v1';
-const urlsToCache = [
-    '/',
-    '/btc/index.html',
-    '/btc/manifest.json',
-    '/btc/icon-192.png',
-    '/btc/icon-512.png',
-    '/btc/service-worker.js',
-    // Add other assets you want to cache
-];
+   self.addEventListener('install', (event) => {
+       event.waitUntil(
+           caches.open('v1').then((cache) => {
+               return cache.addAll([
+                   '/',
+                   '/index.html',
+                   '/manifest.json',
+                   '/icon-192.png',
+                   '/icon-512.png',
+                   // Add other assets you want to cache
+               ]);
+           })
+       );
+   });
 
-// Install the service worker and cache the specified files
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// Fetch event to serve cached files or fetch from network
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Return cached response if available, otherwise fetch from network
-                return response || fetch(event.request);
-            })
-    );
-});
-
-// Activate event to clean up old caches
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+   self.addEventListener('fetch', (event) => {
+       event.respondWith(
+           caches.match(event.request).then((response) => {
+               return response || fetch(event.request);
+           })
+       );
+   });
